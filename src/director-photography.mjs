29ab43e7e781d63f0b676ec6ddb00e81ea582ba9
Alex Perez <apramos89@gmail.com>
2022-02-14 11:49:34 +0100

    Refactor

--- src/director-photography.mjs
+++ src/director-photography.mjs
@@ -5,24 +5,21 @@
  * @param {number} Y
  * @return {number}
  */
-
-function hasScene(type1, type2, type3) {
-  const scene = [type1, type2, type3].join("");
-  return scene === "PAB" || scene === "BAP";
-}
-
 export function getArtisticPhotographCount(N, C, X, Y) {
   let photos = 0;
   for (let i = 0; i < N; i++) {
     for (let j = X; j <= Y; j++) {
       for (let k = X; k <= Y; k++) {
-        if (i + j + k >= N) break;
-        if (hasScene(C[i], C[i + j], C[i + j + k])) {
-          photos++;
-        }
+        if (i + j + k >= N) continue;
+        if (hasScene(C[i], C[i + j], C[i + j + k])) photos++;
       }
     }
   }
 
   return photos;
+}
+
+function hasScene(i, j, k) {
+  const scene = [i, j, k].join("");
+  return scene === "PAB" || scene === "BAP";
 }
