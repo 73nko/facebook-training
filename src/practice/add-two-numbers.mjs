9bad005e29fd4a4fa76bf52242cc21348ff68c0c
Alex Perez <apramos89@gmail.com>
2022-03-05 10:14:19 +0100

    Practice algorithms: add two numbers

---
+++ src/practice/add-two-numbers.mjs
@@ -0,0 +1,33 @@
+export function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+export function addTwoNumbers(l1, l2) {
+  let sum = 0;
+  let current = new ListNode(0);
+  const result = current;
+
+  while (l1 || l2) {
+    if (l1) {
+      sum += l1.val;
+      l1 = l1.next;
+    }
+
+    if (l2) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+
+    current.next = new ListNode(sum % 10);
+    current = current.next;
+
+    sum = sum > 9 ? 1 : 0;
+  }
+
+  if (sum) {
+    current.next = new ListNode(sum);
+  }
+
+  return result.next;
+}
